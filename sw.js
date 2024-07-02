584114b2894cf2235d952ab4137f43d3f0a41b25
graxxku <graxx.sehat@gmail.com>
2024-07-02 19:17:32 +0700

    tes

--- sw.js
+++ sw.js
@@ -1,47 +1,33 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+// (A) CREATE/INSTALL CACHE
+self.addEventListener("install", evt => {
     self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+    evt.waitUntil(
+      caches.open("Demo")
+      .then(cache => cache.addAll([
+        "/",
+        "index.html",
+        "manifest.json",
+        "/models/model.json",
+        "/assets/css/style.css",
+        "/assets/js/build.js",
+        "/assets/js/classes.js",
+        "/assets/js/predict.js",
+        "/assets/media/default_image.jpeg",
+        "/assets/media/logo.png",
+      ]))
+      .catch(err => console.error(err))
+    );
+  });
+  
+  // (B) CLAIM CONTROL INSTANTLY
+  self.addEventListener("activate", evt => self.clients.claim());
+  
+  // (C) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
+  self.addEventListener("fetch", evt => evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
+  ));
+  
+  /* (C) LOAD WITH NETWORK FIRST, FALLBACK TO CACHE IF OFFLINE
+  self.addEventListener("fetch", evt => evt.respondWith(
+    fetch(evt.request).catch(() => caches.match(evt.request))
+  ));*/
