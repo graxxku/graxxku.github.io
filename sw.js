9049c8e773eaec99b55d3f6fcda1ec7fc7977382
graxxku <graxx.sehat@gmail.com>
2024-07-17 15:50:56 +0700

    nocache

--- sw.js
+++ sw.js
@@ -31,11 +31,11 @@ self.addEventListener("install", evt => {
   self.addEventListener("activate", evt => self.clients.claim());
   
   // (C) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
-  self.addEventListener("fetch", evt => evt.respondWith(
-    caches.match(evt.request).then(res => res || fetch(evt.request))
-  ));
+  // self.addEventListener("fetch", evt => evt.respondWith(
+  //   caches.match(evt.request).then(res => res || fetch(evt.request))
+  // ));
   
-  /* (C) LOAD WITH NETWORK FIRST, FALLBACK TO CACHE IF OFFLINE
+  // (C) LOAD WITH NETWORK FIRST, FALLBACK TO CACHE IF OFFLINE
   self.addEventListener("fetch", evt => evt.respondWith(
     fetch(evt.request).catch(() => caches.match(evt.request))
-  ));*/
+  ));
