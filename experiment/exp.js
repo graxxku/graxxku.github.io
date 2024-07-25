73f3662e04dab096b9f854d06ba879ac61422030
graxxku <graxx.sehat@gmail.com>
2024-07-25 19:09:51 +0700

    tes

---
+++ experiment/exp.js
@@ -0,0 +1,19 @@
+const SERVER_URL = "https://graxxku.github.io/";
+
+CLASSES = {
+    0: "Pneuomonia (Bacterial)",
+    1: "Normal (No Pneuomonia)",
+    2: "Pneuomonia (Viral)",
+  };
+  
+
+  let model;
+  (async function () {
+    model = await tf.loadModel(`${SERVER_URL}/experiment/model.json`); // load the model using tensorflow.js
+  })();
+
+
+async function predict() {
+    console.log("tes");
+
+}
