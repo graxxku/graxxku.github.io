3d63d0e1b4a2e19a604721205f338ecc80bf6de1
graxxku <graxx.sehat@gmail.com>
2024-09-25 17:25:29 +0700

    hh

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./lib/**/*.{html,js}","./pages/**/*.{html,js}","./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
